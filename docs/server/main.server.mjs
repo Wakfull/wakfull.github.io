1d585f4f0de01a6c137e99718f791c2dc2a73a7d
Aekami <Aekami@users.noreply.github.com>
2024-08-02 14:04:50 +0200

    - new build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TSBFC65N.mjs";import"./chunk-WZCIZUTA.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-KIVCPLKW.mjs";import"./chunk-WZCIZUTA.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
