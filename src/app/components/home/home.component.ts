ce74bd01907e35b920783a15170c527be0b65948
Aekami <Aekami@users.noreply.github.com>
2024-08-02 14:03:35 +0200

    - Changed images urls to smaller 255*255 versions of same image. May pick images depending on viewport size later.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,16 +12,16 @@ import { PreviewComponent } from './preview/preview.component';
 })
 export class HomeComponent {
   
-  thinkingElio = "https://i.imgur.com/wxZRTLQ.png";
+  thinkingElio = "https://i.imgur.com/WoWn4Bn.png";
   bouftouRoyalUrl = "https://i.imgur.com/CUl0FDk.png";
   royalMoogrUrl ="https://i.imgur.com/DNyK0ep.png";
   dardevilImageUrl = "https://i.imgur.com/RvZ3Ndb.png";
   magmogImageUrl ="https://i.imgur.com/JR9e88l.png";
-  kamasSymbolImageUrl ="https://i.imgur.com/tyCHcEm.png";
+  kamasSymbolImageUrl ="https://i.imgur.com/JwAbjzf.png";
 
 
   previews: {title?:string,description?:string, link?:string, backgroundImage?:string}[] = [
-    { title: "Bien débuter", description: "Tout ce que j'aurais aimé savoir quand j'ai commencé, en vrac", link: "/eldersWisdom",backgroundImage: this.thinkingElio },
+    { title: "Bien débuter", description: "Tout ce que j'aurais aimé savoir quand j'ai commencé en vrac !", link: "/eldersWisdom",backgroundImage: this.thinkingElio },
     { title: "Astuces kamas", description: "Des astuces kamas en vrac !", link: "/kamas", backgroundImage: this.kamasSymbolImageUrl },
     { title: "Stuffs 20", description: "Besoin de stuffs pour débuter, reprendre ou tryhard ? Commence  ici !", link: "/stuff20", backgroundImage: this.bouftouRoyalUrl},
     { title: "Stuffs 50", description: "Le donjon Moogr et la tour minérale n'attendent que toi !", link: "/stuff50", backgroundImage: this.royalMoogrUrl},
