eeea03c688d609500d8116a4a9117edd5176e319
Aekami <Aekami@users.noreply.github.com>
2024-08-02 03:46:37 +0200

    - added content to eldersWisdom

--- src/app/components/elders-wisdom/elders-wisdom.component.ts
+++ src/app/components/elders-wisdom/elders-wisdom.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-elders-wisdom',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './elders-wisdom.component.html',
   styleUrl: './elders-wisdom.component.css'
 })
