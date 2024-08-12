a358cbec3a73eed9b8aab0c39055911b1a57a611
Aekami <Aekami@users.noreply.github.com>
2024-08-12 04:36:08 +0200

    - changed config to allow navigation to fragments (aka, clicking somepage#someElementId now correctly scrolls to selected element)

--- src/app/components/kamas-tips/kamas-tips.component.ts
+++ src/app/components/kamas-tips/kamas-tips.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-kamas-tips',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './kamas-tips.component.html',
   styleUrl: './kamas-tips.component.css'
 })
