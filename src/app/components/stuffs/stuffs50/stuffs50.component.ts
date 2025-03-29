83c36fe6324a2efe54f445dccb3412811673d587
Aekami <Aekami@users.noreply.github.com>
2025-03-29 03:59:20 +0100

    - Refactored DungeonService |-- Added function to return dg from only a specified competitive level - Added prototype "banner" in Stuffs50 which shows the dungeons for that level
    - Moved some CSS to global styles.css to reuse the same classes through components instead of having 10 bajilions variations of the same css.( will refactor once again to use specialized components later)
    
    - Added new placeholder_512 image in public

--- src/app/components/stuffs/stuffs50/stuffs50.component.ts
+++ src/app/components/stuffs/stuffs50/stuffs50.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DungeonService } from '../../../services/dungeon.service';
+import { DungeonItem } from '../../../interfaces/iDungeonItem';
+
 
 @Component({
   selector: 'app-stuffs50',
@@ -7,6 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: './stuffs50.component.html',
   styleUrl: './stuffs50.component.css'
 })
-export class Stuffs50Component {
+export class Stuffs50Component implements OnInit {
 
+  dungeons_50:DungeonItem[] = [];
+  dgService: DungeonService;
+
+  constructor( dgService: DungeonService){
+    this.dgService = dgService;
+  }
+
+  ngOnInit(): void {
+    this.dungeons_50 = this.dgService.getDungeonsAtGivenLevel(50);
+  }
 }
---
+++ src/app/interfaces/iDungeonItem.ts
@@ -0,0 +1,13 @@
+export interface DungeonItem {
+
+    name:string,
+    level:Number,
+    competitive_level:Number,
+    nbr_rooms:Number,
+    nbr_players:Number,
+    is_UB:Boolean,
+    siteUrl:string,
+    backgroundImage?:string,
+    description?:string
+    
+}
