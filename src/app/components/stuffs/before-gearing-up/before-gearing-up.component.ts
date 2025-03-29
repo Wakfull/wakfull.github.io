d81d0c2b87bc191eeddec3f7736d78b126cdd39e
Aekami <Aekami@users.noreply.github.com>
2025-03-29 16:16:38 +0100

    - small visual refactor + resfresh of the Before-Gearing page

--- src/app/components/stuffs/before-gearing-up/before-gearing-up.component.ts
+++ src/app/components/stuffs/before-gearing-up/before-gearing-up.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { AlternativeItemComponent } from '../../../reusables/alternative-item/alternative-item.component';
+import { AlternativeItem } from '../../../interfaces/iAlternativeItem';
 
 @Component({
   selector: 'app-before-gearing-up',
   standalone: true,
-  imports: [],
+  imports: [AlternativeItemComponent],
   templateUrl: './before-gearing-up.component.html',
   styleUrl: './before-gearing-up.component.css'
 })
 export class BeforeGearingUpComponent {
 
-  imgExLardanteUrl = "https://i.imgur.com/xa4xOjk.png";
+  imgExLardanteUrl = "https://i.imgur.com/4mfyTN7.png";
 }
