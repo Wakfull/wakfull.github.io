1be61e832de8a19b58ad0503f33a0413b4b85d58
Aekami <Aekami@users.noreply.github.com>
2026-03-27 18:18:33 +0100

    - Build after update to Angular20

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 2164, hash: '8b3c54a82d2af6600e04fe03151b84b86985fc7cfcdcf1ffde17ddb85d983d79', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1895, hash: '5af3400477f13931a6edc70a15cbe271723f726eb40a196437818f7548d19f00', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2164, hash: '63253d99c327e120e1618fcd333350771521d07e56544d6c0e782258bb254ed9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1895, hash: '1926eb72fcc12e342808de19857cd1457572bd64f8dabe02133d78efec3177f8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-Q5UUTVMW.css': {size: 2612, hash: 'q2kAIRw+XvY', text: () => import('./assets-chunks/styles-Q5UUTVMW_css.mjs').then(m => m.default)}
   },
 };
