cb0f4537fcd3b97c81ebd2343edef19474815859
Aekami <Aekami@users.noreply.github.com>
2026-03-27 18:46:46 +0100

    - Build after update to Angular21

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 2164, hash: '63253d99c327e120e1618fcd333350771521d07e56544d6c0e782258bb254ed9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1895, hash: '1926eb72fcc12e342808de19857cd1457572bd64f8dabe02133d78efec3177f8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2164, hash: '78340bcff163ffdced2a4ba018f657840c85dcb425db02b41dda91779c07b191', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1895, hash: '1bbfeb97718b0b2bdf74fbd4b05ba885e05fce747d627132698f23ee49da2be4', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-Q5UUTVMW.css': {size: 2612, hash: 'q2kAIRw+XvY', text: () => import('./assets-chunks/styles-Q5UUTVMW_css.mjs').then(m => m.default)}
   },
 };
